0e563acfe4b54fe2628e4bb905d3d6251710cd9a
Dave Conley <realworld666@gmail.com>
2017-08-29 14:07:19 +0100

    Done!

--- RoslynTest/Program.cs
+++ RoslynTest/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace RoslynTest
 {
@@ -34,6 +35,10 @@ namespace RoslynTest
                 MetadataReference.CreateFromFile(string.Format(runtimePath, "System.Core"))
             };
 
+        private const string MyModelPath = @"D:\OneDrive\Documents\MMSaveEditor\Model";
+        private const string SourcePath = @"D:\OneDrive\Documents\MM_Decompiled2\Assembly-CSharp";
+        private static StreamWriter outputFile;
+
         private static readonly CSharpCompilationOptions DefaultCompilationOptions =
             new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
                 .WithOverflowChecks(true).WithOptimizationLevel(OptimizationLevel.Release)
@@ -47,23 +52,84 @@ namespace RoslynTest
 
         static void Main(string[] args)
         {
-            var fileToCompile = @"d:\od\onedrive\documents\visual studio 2017\Projects\RoslynTest\RoslynTest\Program.cs";
-            var source = File.ReadAllText(fileToCompile);
-            var parsedSyntaxTree = Parse(source, "", CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp5));
+            var ext = new List<string> { ".cs" };
+            var myFiles = Directory.GetFiles(MyModelPath, "*.*", SearchOption.AllDirectories)
+                 .Where(s => ext.Contains(Path.GetExtension(s)));
 
-            var compilation
-                = CSharpCompilation.Create("Test.dll", new SyntaxTree[] { parsedSyntaxTree }, DefaultReferences, DefaultCompilationOptions);
-            try
-            {
-                var result = compilation.Emit(@"c:\temp\Test.dll");
+            var Mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
 
-                Console.WriteLine(result.Success ? "Sucess!!" : "Failed");
-            }
-            catch (Exception ex)
+            outputFile = new StreamWriter("ParseOutput.txt");
+
+            int count = 0;
+            foreach (string file in myFiles)
             {
-                Console.WriteLine(ex);
+                string fileName = Path.GetFileName(file);
+                if (!File.Exists(Path.Combine(SourcePath, fileName)))
+                {
+                    outputFile.WriteLine(Path.Combine(SourcePath, fileName) + " does not exist");
+                    continue;
+                }
+                // My version of the file
+                var source = File.ReadAllText(file);
+                var parsedSyntaxTree = Parse(source, "", CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp5));
+                var compilation = CSharpCompilation.Create("MyCompilation", syntaxTrees: new[] { parsedSyntaxTree }, references: new[] { Mscorlib });
+                var root = (CompilationUnitSyntax)parsedSyntaxTree.GetRoot();
+                var walker = new SyntaxWalker(compilation.GetSemanticModel(parsedSyntaxTree), fileName);
+                walker.Visit(root);
+
+                // Original version of the file
+                var original = File.ReadAllText(Path.Combine(SourcePath, fileName));
+                var parsedSyntaxTree2 = Parse(original, "", CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp5));
+                var compilation2 = CSharpCompilation.Create("MyCompilation", syntaxTrees: new[] { parsedSyntaxTree2 }, references: new[] { Mscorlib });
+                var root2 = (CompilationUnitSyntax)parsedSyntaxTree2.GetRoot();
+                var walker2 = new SyntaxWalker(compilation2.GetSemanticModel(parsedSyntaxTree2), fileName);
+                walker2.Visit(root2);
+
+                //Console.WriteLine(string.Format("Parsed {0} of {1}", ++count, myFiles.Count()));
+
+                GoCompare(walker, walker2);
+
+
             }
+            Console.WriteLine("Done");
             Console.Read();
+        }
+
+        private static void GoCompare(SyntaxWalker walker, SyntaxWalker walker2)
+        {
+            bool printedHeader = false;
+            if (walker.DefinedFields.Count == walker2.DefinedFields.Count || walker.DefinedFields.Count > walker2.DefinedFields.Count)
+            {
+                foreach (var myField in walker.DefinedFields)
+                {
+                    if (!walker2.DefinedFields.Any(f => f.name.Equals(myField.name) && f.type.Equals(myField.type)))
+                    {
+                        if (!printedHeader)
+                        {
+                            printedHeader = true;
+                            outputFile.WriteLine(walker2.FileName + ":");
+                        }
+                        outputFile.WriteLine(string.Format("{0} {1} {2};", myField.accessibility, myField.type, myField.name));
+                    }
+                }
+            }
+            if (walker.DefinedFields.Count == walker2.DefinedFields.Count || walker.DefinedFields.Count < walker2.DefinedFields.Count)
+            {
+                foreach (var myField in walker2.DefinedFields)
+                {
+                    if (!walker.DefinedFields.Any(f => f.name.Equals(myField.name) && f.type.Equals(myField.type)))
+                    {
+                        if (!printedHeader)
+                        {
+                            printedHeader = true;
+                            outputFile.WriteLine(walker.FileName + ":");
+                        }
+                        outputFile.WriteLine(string.Format("{0} {1} {2};", myField.accessibility, myField.type, myField.name));
+                    }
+                }
+            }
+
+            outputFile.WriteLine();
         }
     }
 }
---
+++ RoslynTest/SyntaxWalker.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+
+namespace RoslynTest
+{
+    class SyntaxWalker : CSharpSyntaxWalker
+    {
+        private readonly SemanticModel _model;
+
+        public class FieldDef
+        {
+            public string type;
+            public string name;
+            public string accessibility;
+        }
+        public List<FieldDef> DefinedFields = new List<FieldDef>();
+        private string _name;
+
+        public string FileName
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public SyntaxWalker(SemanticModel model, string filename)
+        {
+            _model = model;
+            _name = filename;
+        }
+
+
+        public override void VisitFieldDeclaration(FieldDeclarationSyntax node)
+        {
+            base.VisitFieldDeclaration(node);
+
+            foreach (var variable in node.Declaration.Variables)
+            {
+                var fieldSymbol = _model.GetDeclaredSymbol(variable);
+
+                DefinedFields.Add(new FieldDef()
+                {
+                    type = node.Declaration.Type.ToString(),
+                    name = fieldSymbol.Name,
+                    accessibility = fieldSymbol.DeclaredAccessibility.ToString().ToLower()
+                });
+            }
+        }
+    }
+}
